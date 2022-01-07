5ba78b764e31b1f9a7f5c0225a7b086143164f5c
Jesus Conde <educaleft@gmail.com>
2022-01-07 09:21:56 +0000

    Agregar archivos de proyecto.

---
+++ SobrecargaFunciones/SobrecargaFunciones.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int sumar (int x, int y)
+{
+	return x + y;
+}
+
+
+int sumarInts(int x, int y)
+{
+	return x + y;
+}
+
+int sumarDoubles(double x, double y)
+{
+	return x + y;
+}
+
+double sumar(double x, double y)
+{
+	return x + y;
+}
+
+int sumar(int x, int y, int z)
+{
+	return x + y + z;
+}
+
+int main()
+{
+	int a = sumar(3, 5);
+	std::cout << a <<"\n";
+	double b = sumar(4.3, 5.6);
+	std::cout << b << "\n";
+	int c = sumar(6, 7, 5);
+	std::cout << c << "\n";
+}
+
+
+
+
+
+
